1505b0cbe9085d1f36eee50dd8e164a3d71bc80a
Stam Kaly <stambugatti@gmail.com>
2016-07-09 22:34:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import get.db as db
+import get.dbhash as db.hash
+import get.localip as localip
+import get.localt as localt
+import get.timezone as timezone
+
+
+
+print(localip.localip())
+print(timezone.local())
+print(timezone.timezone("37.6.241.211"))
+# localt (= local time) doesnt work yet
