8d2e4952c06090175c1f0556765b32b3eeb2f1b0
Stam Kaly <stambugatti@gmail.com>
2016-07-10 15:18:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ db.download()
 print(localip.localip())
 print(timezone.local())
 print(timezone.timezone("37.6.241.211"))
-print(localt.ip2tz("37.6.241.211"))
+print(localt.ip2lt("37.6.241.211"))
 
 
