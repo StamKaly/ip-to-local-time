a9f265febe21dffcfef66fd87e30e4469d1b54a0
Stam Kaly <stambugatti@gmail.com>
2016-07-11 18:43:18 +0300

    Update testing-for-dst.py

--- get/testing-for-dst.py
+++ get/testing-for-dst.py
@@ -1,20 +1,20 @@
 import pytz
 import datetime
-from pytz import timezone
 from datetime import datetime
+import time
+
+local_time = time.localtime()
 
 # Two times to test, one in DST, one not
-dst_time = datetime(2016,2,4, 3,0,0, tzinfo=pytz.utc)
-nondst_time = datetime(2016,8,4, 3,0,0, tzinfo=pytz.utc)
+sometime = datetime(local_time.tm_year, local_time.tm_mon, local_time.tm_mday, local_time.tm_hour, local_time.tm_min,
+                    local_time.tm_sec, tzinfo=pytz.utc)
 
 # The timezone we want to convert to, which sometimes uses DST
-target = timezone('US/Eastern')
+target = pytz.timezone('Europe/London')
 
 # Convert the times to the timezone, adjusing for dst
-dst_converted = target.normalize(dst_time.astimezone(target));
-nondst_converted = target.normalize(nondst_time.astimezone(target));
+sometime_converted = target.normalize(sometime.astimezone(target));
 
-fmt = '%H%M'
 
 print(dst_converted)
 print(nondst_converted)
