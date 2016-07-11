c7a3318a2561b3ef2ac2a535c35cf0998cb6f214
StamKaly <stambugatti@gmail.com>
2016-07-11 21:59:55 +0300

    hopefully everything works

---
+++ get/dst.py
@@ -0,0 +1,28 @@
+from . import timezone
+import pytz
+from datetime import datetime
+import time
+
+
+
+def dst_timeconvert(ip, hours, minutes):
+    target_tz = pytz.timezone(timezone.timezone(ip))
+    utcdate = time.gmtime()
+    target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, hours, minutes, 0, tzinfo=pytz.utc)
+    timestr = str(target_dt)
+    dt, t = timestr.split(' ')
+    tim = t[:5]
+    return tim
+	
+	
+	
+
+def dst_convert(ip):
+    target_tz = pytz.timezone(timezone.timezone(ip))
+    utcdate = time.gmtime()
+    target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, utcdate.tm_hour, utcdate.tm_min, 0, tzinfo=pytz.utc)
+    timestr = str(target_dt)
+    dt, t = timestr.split(' ')
+    tim = t[:5]
+    return tim
+
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from get import *
+from get import dst
 
 
 
 
-print(localip.localip())
-print(timezone.local())
-print(timezone.timezone("37.6.241.211"))
-print(localt.ip2lt("37.6.241.211"))
+print(dst.dst_convert("37.6.241.211"))
