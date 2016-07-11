9c4aad281d163381897e202bc8157750694ff95c
StamKaly <stambugatti@gmail.com>
2016-07-11 23:02:54 +0300

    updated main.py

--- get/localt.py
+++ get/localt.py
@@ -4,31 +4,40 @@ import pytz
 import time
 import sys
 
-def ip2lt(ip):
-	try:
-		target_tz = pytz.timezone(timezone.timezone(ip))
-	except pytz.exceptions.UnknownTimeZoneError:
-		print("Unable to detect given IP's time zone, try using\n'localt.ip2lt2(time zone)' instead")
-        	sys.exit(0)
-    utcdate = time.gmtime()
-    target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, utcdate.tm_hour, utcdate.tm_min, 0, tzinfo=pytz.utc)
-    conversion = target_tz.normalize(target_dt.astimezone(target_tz))
-    timestr = str(conversion)
-    dt, t = timestr.split(' ')
-    tim = t[:5]
-    return tim
 
+def ip2lt(ip):
+    try:
+        try:
+            target_tz = pytz.timezone(timezone.timezone(ip))
+        except pytz.exceptions.UnknownTimeZoneError:
+            print("Unable to detect given IP's time zone, try using\n'localt.ip2lt2(time zone)' instead")
+            sys.exit(0)
+        utcdate = time.gmtime()
+        target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, utcdate.tm_hour, utcdate.tm_min, 0, tzinfo=pytz.utc)
+        conversion = target_tz.normalize(target_dt.astimezone(target_tz))
+        timestr = str(conversion)
+        dt, t = timestr.split(' ')
+        tim = t[:5]
+        return tim
+    except ValueError:
+        print("Please enter a valid IP address!")
+        sys.exit(0)
+    
 
 def ip2lt2(time_zone):
-	try:
-		target_tz = pytz.timezone(time_zone)
-	except pytz.exceptions.UnknownTimeZoneError:
-		print("Unable to detect given IP's time zone, try using\n'localt.ip2lt2(time zone)' instead")
-        	sys.exit(0)
-    utcdate = time.gmtime()
-    target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, utcdate.tm_hour, utcdate.tm_min, 0, tzinfo=pytz.utc)
-    conversion = target_tz.normalize(target_dt.astimezone(target_tz))
-    timestr = str(conversion)
-    dt, t = timestr.split(' ')
-    tim = t[:5]
-    return tim
+    try:
+        try:
+            target_tz = pytz.timezone(time_zone)
+        except pytz.exceptions.UnknownTimeZoneError:
+            print("Unable to detect given time zone, please try again!")
+            sys.exit(0)
+        utcdate = time.gmtime()
+        target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, utcdate.tm_hour, utcdate.tm_min, 0, tzinfo=pytz.utc)
+        conversion = target_tz.normalize(target_dt.astimezone(target_tz))
+        timestr = str(conversion)
+        dt, t = timestr.split(' ')
+        tim = t[:5]
+        return tim
+    except ValueError:
+        print("Please enter a valid IP address!")
+        sys.exit(0)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from get import dst
+from get import *
 
 
 
 
-print(dst.dst_convert("37.6.241.211"))
+print(localt.ip2lt("37.6.241.211"))
+print(localt.ip2lt2("US/Eastern"))
