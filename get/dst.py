92ec25758eabdaec824e445f42f71128b686dc15
StamKaly <stambugatti@gmail.com>
2016-07-11 22:10:32 +0300

    fixed stupid bug

--- get/dst.py
+++ get/dst.py
@@ -9,7 +9,8 @@ def dst_timeconvert(ip, hours, minutes):
     target_tz = pytz.timezone(timezone.timezone(ip))
     utcdate = time.gmtime()
     target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, hours, minutes, 0, tzinfo=pytz.utc)
-    timestr = str(target_dt)
+	conversion = target_tz.normalize(target_dt.astimezone(target_tz))
+    timestr = str(conversion)
     dt, t = timestr.split(' ')
     tim = t[:5]
     return tim
@@ -21,7 +22,8 @@ def dst_convert(ip):
     target_tz = pytz.timezone(timezone.timezone(ip))
     utcdate = time.gmtime()
     target_dt = datetime(utcdate.tm_year, utcdate.tm_mon, utcdate.tm_mday, utcdate.tm_hour, utcdate.tm_min, 0, tzinfo=pytz.utc)
-    timestr = str(target_dt)
+	conversion = target_tz.normalize(target_dt.astimezone(target_tz))
+    timestr = str(conversion)
     dt, t = timestr.split(' ')
     tim = t[:5]
     return tim
